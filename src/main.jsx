ac272cc4b267ca0c9bc11c3029298adfcc79d6fe
Chabane Nadji <chabane.nadji@gmail.com>
2025-07-31 16:37:09 +0200

    Implementation of the Feature section

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,7 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
-import * as Sentry from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://a34d88d424559848e7eeded22fea3499@o4509752044945408.ingest.de.sentry.io/4509752046059600",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
-  sendDefaultPii: true,
-});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
